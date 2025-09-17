f6aaabaac0f8fa11032d801d580d7009b46ed5a0
Umasri537 <gubbalaumasri4@gmail.com>
2025-09-17 14:02:00 +0530

    useRef-Focus

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useRef } from "react"
+import TodoList from "./Todolist"
 function App() {
-  var aRef=useRef();
-  var bRef=useRef();
-  function handleClick(){
-    var x=aRef.current.value;
-    bRef.current.innerText=x;
-  }
-  useEffect(()=>{
-    aRef.current.focus();
-  },[]);
-  
+  // var aRef=useRef();
+  // var bRef=useRef();
+  // function handleClick(){
+  //   var x=aRef.current.value;
+  //   bRef.current.innerText=x;
+  // }
+  // useEffect(()=>{
+  //   aRef.current.focus();
+  // },[]);
+
   return (
     <div className="border border-3 border-secondary p-3 m-3">
-       <h1>useRef Component:</h1>
+       {/* <h1>useRef Component:</h1>
        <input type="text" ref={aRef}/>
        <button onClick={()=>{handleClick();}}>ShowText</button>
-       <h3>Myname:<span ref={bRef}></span></h3> 
+       <h3>Myname:<span ref={bRef}></span></h3>  */}
+       <h1>useRef Focus Component</h1>
+       <TodoList></TodoList>
     </div>
     
   )
